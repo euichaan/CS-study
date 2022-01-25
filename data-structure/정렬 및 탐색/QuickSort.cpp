157d84cd63b8010991dbcf4895f5d36115904ab1
chanbest <b611219@g.hongik.ac.kr>
2022-01-25 18:13:36 +0900

    quicksort 소스파일과 설명 추가

--- data-structure/정렬 및 탐색/QuickSort.cpp
+++ data-structure/정렬 및 탐색/QuickSort.cpp
@@ -1,9 +1,48 @@
 #include<iostream>
 //Data Structure
 //quick sort 구현 
+int number = 10;
+int data[10]= {1,10,5,8,7,6,4,3,2,9};
+void quickSort(int *data,int start,int end)
+{
+    if(start>=end) return; //원소가 1개인 경우
+    int key=start; //키는 첫번째 원소
+    int i=start+1; //i 는 왼쪽 출발지점
+    int j=end; // j 는 오른쪽 출발지점
+    int temp;
+
+    while(i<=j)
+    {
+        while(data[i]<=data[key])
+        {
+            i++;
+        }
+        while(data[j]>=data[key] && j>start)
+        {
+            j--; 
+        }
+        if(i>j)
+        {
+            temp=data[j];
+            data[j]=data[key];
+            data[key]=temp;
+        }
+        else{
+            temp=data[j];
+            data[j]=data[i];
+            data[i]=temp;
+        }
+    }
+    quickSort(data,start,j-1);
+    quickSort(data,j+1,end);
+    
+}
 int main(void)
 {
-
+    quickSort(data,0,number-1);
+     for(auto& e:data)
+      std::cout << e << " ";
+    std::cout<<std::endl;
 
 
     return 0;
